d4e52ac18315243593400a818c950d6373522caf
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-09-03 17:22:04 +0200

    chore: remove main and devenv and added flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls          # Go language server (optional but recommended)
+            go-tools       # Additional tools like staticcheck
+          ];
+        };
+      });
+}
