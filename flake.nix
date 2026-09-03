af431a6fe6e029357c2221c69922850fabbfecb4
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-09-03 17:42:53 +0200

    feat: adding aspire to my project

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    aspire = {
+      url = "github:microsoft/aspire";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, aspire, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -14,6 +18,7 @@
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            aspire.packages.${system}.aspire-cli  # Use from aspire flake, not nixpkgs
             go
             gopls          # Go language server (optional but recommended)
             go-tools       # Additional tools like staticcheck
